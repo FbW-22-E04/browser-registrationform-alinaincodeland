7783dbdf93cc46a918f8a1f105b3635a57711fb8
dci-student <alinacuznetov@gmail.com>
2022-10-06 16:14:45 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 function previous(element) {
   let fatherDiv = element.parentNode.id;
   switch (fatherDiv) {
-    case 'addressDiv':
-      element.parentNode.style.display = 'none';
-      document.getElementById('personalDiv').style.display = 'block';
-      document.getElementById('profileDiv').style.display = 'none';
-      document.getElementById('resultDiv').style.display = 'none';
+    case "addressDiv":
+      element.parentNode.style.display = "none";
+      document.getElementById("personalDiv").style.display = "block";
+      document.getElementById("profileDiv").style.display = "none";
+      document.getElementById("resultDiv").style.display = "none";
       break;
-    case 'profileDiv':
-      element.parentNode.style.display = 'none';
-      document.getElementById('personalDiv').style.display = 'none';
-      document.getElementById('addressDiv').style.display = 'block';
-      document.getElementById('resultDiv').style.display = 'none';
+    case "profileDiv":
+      element.parentNode.style.display = "none";
+      document.getElementById("personalDiv").style.display = "none";
+      document.getElementById("addressDiv").style.display = "block";
+      document.getElementById("resultDiv").style.display = "none";
 
       break;
-    case 'resultDiv':
-      element.parentNode.style.display = 'none';
-      document.getElementById('personalDiv').style.display = 'none';
-      document.getElementById('addressDiv').style.display = 'none';
-      document.getElementById('profileDiv').style.display = 'block';
+    case "resultDiv":
+      element.parentNode.style.display = "none";
+      document.getElementById("personalDiv").style.display = "none";
+      document.getElementById("addressDiv").style.display = "none";
+      document.getElementById("profileDiv").style.display = "block";
 
       break;
 
@@ -26,3 +26,12 @@ function previous(element) {
       break;
   }
 }
+
+const personalDiv = document.querySelector("#personalDiv");
+const addressDiv = document.querySelector("#addressDiv");
+const registerBtn = document.querySelector(".registerbtn");
+
+registerBtn.addEventListener("click", () => {
+  personalDiv.style.display = "none";
+  addressDiv.style.display = "block";
+});
